5cee3f8262b40a8ea3b967407ff78f9c3cd8b9b1
mayanklkumar87 <33685474+mayanklkumar87@users.noreply.github.com>
2019-01-14 00:21:43 +0530

    Added Comments

--- src/pkg/cache/basic/CacheEntry.java
+++ src/pkg/cache/basic/CacheEntry.java
@@ -1,10 +1,17 @@
 package pkg.cache.basic;
 
+/**
+* CacheEntry is the node of doubly linked list to which cache map keys point
+*/
 public class CacheEntry {
-	
+		/** Cache Object value */
 		Object value;
+		/** Cache Object key */
 		int key;
+		/** previous Cache Object reference */
 		CacheEntry prev;
+		/** next Cache Object reference */
 		CacheEntry next;
+		/** last accessed time of current cache entry */
 		long lastAccessedTime;
 }
